bec9bc894bf785dae60c8bb328928013ac031b6a
小天狼x <xingkexin@163.com>
2020-03-08 10:38:59 +0800

    增加百度统计

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+// 以下为百度统计的代码
+var _hmt = _hmt || [];
+window._hmt = _hmt; // 修改为window 全局变量
+if(process.env.NODE_ENV != 'development') {
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?5e32c08d95854b2989a0ac08477b3494";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+  _hmt.push(['_requirePlugin', 'UrlChangeTracker', {
+    shouldTrackUrlChange: function (newPath, oldPath) {
+    return newPath && oldPath;
+    }}
+  ]);
+}
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
